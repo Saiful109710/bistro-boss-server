05504cd0fef8188e1e8c90dc65d5b9b88eb9aa83
saiful-islam <saifulislam109710@gmail.com>
2025-01-13 00:44:48 +0600

    add cart api

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ async function run() {
 
     const menuCollection = client.db('bistroDB').collection('menu')
     const reviewCollection = client.db('bistroDB').collection('reviews')
+    const cartCollection = client.db('bistroDB').collection('carts')
 
     app.get('/menus',async(req,res)=>{
         const result = await menuCollection.find().toArray()
@@ -41,6 +42,15 @@ async function run() {
     app.get('/reviews',async(req,res)=>{
         const result = await reviewCollection.find().toArray()
         res.send(result)
+    })
+
+    // carts collection
+
+    app.post('/carts',async(req,res)=>{
+      const cartItem = req.body
+      const result = await cartCollection.insertOne(cartItem)
+      res.send(result)
+
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
