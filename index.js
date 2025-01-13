1ea3f5fcb03737e4fd65574245b3341f2a51e51e
saiful-islam <saifulislam109710@gmail.com>
2025-01-14 00:54:47 +0600

    add delete api cart

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000
@@ -45,12 +45,24 @@ async function run() {
     })
 
     // carts collection
-
+    app.get('/carts',async(req,res)=>{
+      const email = req.query.email
+      const query = {email:email}
+      const result = await cartCollection.find(query).toArray()
+      res.send(result) 
+    })
     app.post('/carts',async(req,res)=>{
       const cartItem = req.body
       const result = await cartCollection.insertOne(cartItem)
       res.send(result)
 
+    })
+
+    app.delete('/carts/:id',async(req,res)=>{
+        const id  = req.params.id
+        const query = {_id:new ObjectId(id)}
+        const result = await cartCollection.deleteOne(query)
+        res.send(result)
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
