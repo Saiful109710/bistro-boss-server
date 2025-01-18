63813d11f3a3612d58d11fbaef81668b4674210c
saiful-islam <saifulislam109710@gmail.com>
2025-01-18 12:58:55 +0600

    fix bug

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ async function run() {
     })
     // delete user from db
 
-    app.delete('/users/:id',verifyAdmin,verifyToken,async(req,res)=>{
+    app.delete('/users/:id',verifyToken,verifyAdmin,async(req,res)=>{
       const id = req.params.id
       const query = {_id:new ObjectId(id)}
       const result = await userCollection.deleteOne(query)
